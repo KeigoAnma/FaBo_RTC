47663a2179470a32e103f656bbdf15f6b863c60c
Ikumi-Otani <matelediter@gmail.com>
2017-01-19 17:19:57 +0900

    Add 205 Proximity

---
+++ 205_Proximity/Proximity.py
@@ -0,0 +1,310 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# -*- Python -*-
+
+"""
+ @file Proximity.py
+ @brief ModuleDescription
+ @date $Date$
+
+
+"""
+import sys
+import time
+sys.path.append(".")
+
+# Import RTM module
+import RTC
+import OpenRTM_aist
+
+#Import FaBo 205 Proximity library
+import FaBoProximity_VCNL4010 as vcnl
+
+# Import Service implementation class
+# <rtc-template block="service_impl">
+
+# </rtc-template>
+
+# Import Service stub modules
+# <rtc-template block="consumer_import">
+# </rtc-template>
+
+
+# This module's spesification
+# <rtc-template block="module_spec">
+proximity_spec = ["implementation_id", "Proximity", 
+		 "type_name",         "Proximity", 
+		 "description",       "ModuleDescription", 
+		 "version",           "1.0.0", 
+		 "vendor",            "VenderName", 
+		 "category",          "Category", 
+		 "activity_type",     "STATIC", 
+		 "max_instance",      "1", 
+		 "language",          "Python", 
+		 "lang_type",         "SCRIPT",
+		 ""]
+# </rtc-template>
+
+##
+# @class Proximity
+# @brief ModuleDescription
+# 
+# 205 Proximity Sensor Unit for Raspberry Pi by Fabo. Sensor module is VCNL4010.
+#	This sensor gets Proximity and Ambient value. If you use this Unit and RTC,
+#	you should connect sensor unit for Raspberry PI's I2C port.
+# 
+# Out: Proximity and Ambient(Type TimedShort).
+# 
+# 
+class Proximity(OpenRTM_aist.DataFlowComponentBase):
+	
+	##
+	# @brief constructor
+	# @param manager Maneger Object
+	# 
+	def __init__(self, manager):
+		OpenRTM_aist.DataFlowComponentBase.__init__(self, manager)
+		
+		#fix
+		#prox_arg = [None] * ((len(RTC._d_TimedShort) - 4) / 2)
+		#self._d_prox = RTC.TimedShort(*prox_arg)
+		self._d_prox = RTC.TimedShort(RTC.Time(0,0),0)
+		"""
+		get value proximity
+		 - Type: short
+		 - Number: 1
+		"""
+		self._ProximityOut = OpenRTM_aist.OutPort("Proximity", self._d_prox)
+		
+		#fix	
+		#ambi_arg = [None] * ((len(RTC._d_TimedShort) - 4) / 2)
+		#self._d_ambi = RTC.TimedShort(*ambi_arg)
+		self._d_ambi = RTC.TimedShort(RTC.Time(0,0),0)
+		"""
+		get value Ambient
+		 - Type: short
+		 - Number: 1
+		"""
+		self._AmbientOut = OpenRTM_aist.OutPort("Ambient", self._d_ambi)
+
+
+		
+
+
+		# initialize of configuration-data.
+		# <rtc-template block="init_conf_param">
+		
+		# </rtc-template>
+		self._vncnl4010 = vcnl.VCNL4010()
+
+		 
+	##
+	#
+	# The initialize action (on CREATED->ALIVE transition)
+	# formaer rtc_init_entry() 
+	# 
+	# @return RTC::ReturnCode_t
+	# 
+	#
+	def onInitialize(self):
+		# Bind variables and configuration variable
+		
+		# Set InPort buffers
+		
+		# Set OutPort buffers
+		self.addOutPort("Proximity",self._ProximityOut)
+		self.addOutPort("Ambient",self._AmbientOut)
+		
+		# Set service provider to Ports
+		
+		# Set service consumers to Ports
+		
+		# Set CORBA Service Ports
+		
+		return RTC.RTC_OK
+	
+	#	##
+	#	# 
+	#	# The finalize action (on ALIVE->END transition)
+	#	# formaer rtc_exiting_entry()
+	#	# 
+	#	# @return RTC::ReturnCode_t
+	#
+	#	# 
+	#def onFinalize(self):
+	#
+	#	return RTC.RTC_OK
+	
+	#	##
+	#	#
+	#	# The startup action when ExecutionContext startup
+	#	# former rtc_starting_entry()
+	#	# 
+	#	# @param ec_id target ExecutionContext Id
+	#	#
+	#	# @return RTC::ReturnCode_t
+	#	#
+	#	#
+	#def onStartup(self, ec_id):
+	#
+	#	return RTC.RTC_OK
+	
+	#	##
+	#	#
+	#	# The shutdown action when ExecutionContext stop
+	#	# former rtc_stopping_entry()
+	#	#
+	#	# @param ec_id target ExecutionContext Id
+	#	#
+	#	# @return RTC::ReturnCode_t
+	#	#
+	#	#
+	#def onShutdown(self, ec_id):
+	#
+	#	return RTC.RTC_OK
+	
+	#	##
+	#	#
+	#	# The activated action (Active state entry action)
+	#	# former rtc_active_entry()
+	#	#
+	#	# @param ec_id target ExecutionContext Id
+	#	# 
+	#	# @return RTC::ReturnCode_t
+	#	#
+	#	#
+	#def onActivated(self, ec_id):
+	#
+	#	return RTC.RTC_OK
+	
+	#	##
+	#	#
+	#	# The deactivated action (Active state exit action)
+	#	# former rtc_active_exit()
+	#	#
+	#	# @param ec_id target ExecutionContext Id
+	#	#
+	#	# @return RTC::ReturnCode_t
+	#	#
+	#	#
+	#def onDeactivated(self, ec_id):
+	#
+	#	return RTC.RTC_OK
+	
+		##
+		#
+		# The execution action that is invoked periodically
+		# former rtc_active_do()
+		#
+		# @param ec_id target ExecutionContext Id
+		#
+		# @return RTC::ReturnCode_t
+		#
+		#
+	def onExecute(self, ec_id):
+		
+		self._d_prox.data = self._vcnl4010.readProx()
+		self._d_ambi.data = self._vcnl4010.readAmbi()
+		print "Prox = ", self._d_prox.data
+		print "Ambi = ", self._d_ambi.data
+
+		self._ProximityOut.write()
+		self._AmbientOut.write()
+
+		return RTC.RTC_OK
+	
+	#	##
+	#	#
+	#	# The aborting action when main logic error occurred.
+	#	# former rtc_aborting_entry()
+	#	#
+	#	# @param ec_id target ExecutionContext Id
+	#	#
+	#	# @return RTC::ReturnCode_t
+	#	#
+	#	#
+	#def onAborting(self, ec_id):
+	#
+	#	return RTC.RTC_OK
+	
+	#	##
+	#	#
+	#	# The error action in ERROR state
+	#	# former rtc_error_do()
+	#	#
+	#	# @param ec_id target ExecutionContext Id
+	#	#
+	#	# @return RTC::ReturnCode_t
+	#	#
+	#	#
+	#def onError(self, ec_id):
+	#
+	#	return RTC.RTC_OK
+	
+	#	##
+	#	#
+	#	# The reset action that is invoked resetting
+	#	# This is same but different the former rtc_init_entry()
+	#	#
+	#	# @param ec_id target ExecutionContext Id
+	#	#
+	#	# @return RTC::ReturnCode_t
+	#	#
+	#	#
+	#def onReset(self, ec_id):
+	#
+	#	return RTC.RTC_OK
+	
+	#	##
+	#	#
+	#	# The state update action that is invoked after onExecute() action
+	#	# no corresponding operation exists in OpenRTm-aist-0.2.0
+	#	#
+	#	# @param ec_id target ExecutionContext Id
+	#	#
+	#	# @return RTC::ReturnCode_t
+	#	#
+
+	#	#
+	#def onStateUpdate(self, ec_id):
+	#
+	#	return RTC.RTC_OK
+	
+	#	##
+	#	#
+	#	# The action that is invoked when execution context's rate is changed
+	#	# no corresponding operation exists in OpenRTm-aist-0.2.0
+	#	#
+	#	# @param ec_id target ExecutionContext Id
+	#	#
+	#	# @return RTC::ReturnCode_t
+	#	#
+	#	#
+	#def onRateChanged(self, ec_id):
+	#
+	#	return RTC.RTC_OK
+	
+
+
+
+def ProximityInit(manager):
+    profile = OpenRTM_aist.Properties(defaults_str=proximity_spec)
+    manager.registerFactory(profile,
+                            Proximity,
+                            OpenRTM_aist.Delete)
+
+def MyModuleInit(manager):
+    ProximityInit(manager)
+
+    # Create a component
+    comp = manager.createComponent("Proximity")
+
+def main():
+	mgr = OpenRTM_aist.Manager.init(sys.argv)
+	mgr.setModuleInitProc(MyModuleInit)
+	mgr.activateManager()
+	mgr.runManager()
+
+if __name__ == "__main__":
+	main()
+
---
+++ 205_Proximity/README.md
@@ -0,0 +1,6 @@
+## Fabo Sensor Brick 205 Proximity  
+RTC Proximity: RTC for Fabo 205 Proximity  
+Sensor Unit connect for Raspberry Pi's I2C Port. 
+*This RTC use python library FaBoProximity_VCNL4010*    
+if you don't have this, please install by pip  
+`pip install FaBoProximity_VCNL4010`
